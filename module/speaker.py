ffc92bb9709976ece8f92a1ddd34f5d514bb3270
kaide <kaide@kaide-iMac-Pro.local>
2018-02-15 02:20:42 +0900

    食べログの検索APIv11

--- module/speaker.py
+++ module/speaker.py
@@ -8,7 +8,7 @@ from decimal import Decimal as D
 from bs4 import BeautifulSoup
 import requests
 
-BASE_URL = 'https://tabelog.com/rstLst/?vs=1&sa=&sk={search_keyword}&lid=hd_search1&vac_net=&svd={dt_string}&svt=1900&svps=2&hfc=1&Cat=RC&LstCat=RC02&LstCatD=RC0212&sw='
+BASE_URL = 'https://tabelog.com/rstLst/?vs=1&sa=&sk={search_keyword}&lid=hd_search1&vac_net=&svd={dt_string}&svt=1900&svps=2&hfc=1&Cat=RC&LstCat=RC02&LstCatD=RC0212&sw={search_prekeyword}'
 
 
 class Talking(object):
@@ -16,10 +16,12 @@ class Talking(object):
     def __init__(self, message):
         self.message = message
         search_keyword = quote(quote(self.message))
+        search_prekeyword = self.message.split()[0]
         dt_string = datetime.now().strftime('%Y%m%d')
         self.url = BASE_URL.format(
             search_keyword=search_keyword,
             dt_string=dt_string,
+            search_prekeyword=search_prekeyword,
         )
 
     def search(self):
